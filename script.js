4b1585694a33e4d0b9c3c536e23b33f292ee9711
BruxoSagaz <umpoucodetudogr@gmail.com>
2022-09-19 00:50:15 -0300

    styling

--- script.js
+++ script.js
@@ -1,53 +1,24 @@
-var cartas = [
-    carta1 = {
-      nome: "Dio Brando",
-      img:
-        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/71fbb00b-be92-4757-8791-0aaae3cc055d/ddo6tso-96deb885-0f72-4f0f-8fb9-0c4e1745b38c.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzcxZmJiMDBiLWJlOTItNDc1Ny04NzkxLTBhYWFlM2NjMDU1ZFwvZGRvNnRzby05NmRlYjg4NS0wZjcyLTRmMGYtOGZiOS0wYzRlMTc0NWIzOGMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.PcycMvPKchBs0cljRX8qUPsp4wftm1UCR_SAzzHEjKc",
-      atributos: {
-        estilo: 999,
-        defesa: 60,
-        ataque: 70
-      }
-    },
-    carta2 = {
-      nome: "Jotaro Kujo",
-      img:
-        "https://freepngimg.com/thumb/anime/118756-jotaro-png-file-hd.png",
-      atributos: {
-        estilo: 500,
-        defesa: 80,
-        ataque: 80
-      }
-    },
-    carta3 = {
-      nome: "Josuke Higashikata",
-      img:
-        "https://64.media.tumblr.com/ded526c0aac7aa9a37e0579dfad30330/9176885704b45ed8-06/s540x810/b74e2790da97b07d5fed6bb8ecfa26b8df1d311c.png",
-      atributos: {
-        estilo: 450,
-        defesa: 90,
-        ataque: 70
-      }
-    },
-    carta4 = {
-      nome: "Kira Yoshikage", img:"https://static.jojowiki.com/images/thumb/b/b2/latest/20210107192532/Yoshikage_Kira_Matured_Infobox_Anime.png/400px-Yoshikage_Kira_Matured_Infobox_Anime.png",
-      atributos: {
-        estilo: 600,
-        defesa: 50,
-        ataque: 90
-      }
-    }
-  ];
+  import cartas from './cartas.json' assert {type: 'json'};
+  // var cartas;
+  // var url = "./cartas.json";         
+  // $.getJSON(url, function (data) {
+  //   cartas = data; 
+  //   console.log(cartas);
+  // });
+  // console.log(cartas);
   var cartaMaquina;
   var cartaJogador;
   var sortearBtn = document.getElementById("btnSortear");
   var jogarBtn = document.getElementById("btnJogar");
-  
-  function sortearCarta() {
-    numero = parseInt(Math.random() * cartas.length);
+  console.log(cartas.length);
+
+  $("#btnSortear").click(function() { 
+    var numero = parseInt(Math.random() * cartas.length);
+    console.log(numero);
     cartaMaquina = cartas[numero];
-  
-    numero2 = parseInt(Math.random() * cartas.length);
+
+    console.log(cartaMaquina);
+    var numero2 = parseInt(Math.random() * cartas.length);
     while (numero2 == numero) {
       numero2 = parseInt(Math.random() * cartas.length);
     }
@@ -56,6 +27,7 @@ var cartas = [
     var cardsArea = $('.hide-cards-area');
     $('.space').fadeOut("fast",function(){
         cardsArea.fadeIn("slow");
+        
         displayCarta("card-place",cartaJogador);
         showOpts();
         sortearBtn.disabled = true;
@@ -63,8 +35,8 @@ var cartas = [
     });
 
 
-    
-  }
+  })
+
   
   function showOpts() {
     var divOpt = document.getElementById("opcoes");
@@ -82,7 +54,7 @@ var cartas = [
   
   function obterInputSelect() {
     var selectedAttr = document.getElementsByName("atributo");
-    for (i = 0; i < selectedAttr.length; i++) {
+    for (var i = 0; i < selectedAttr.length; i++) {
       if (selectedAttr[i].checked) {
         return selectedAttr[i].value;
       }
@@ -91,7 +63,7 @@ var cartas = [
     return false;
   }
   
-  function jogar() {
+  $("#btnJogar").click(function() {
     var atributo = obterInputSelect();
     if(atributo != false){
         var resultDiv = document.getElementById("resultado");
@@ -109,7 +81,7 @@ var cartas = [
       // Empate
       resultDiv.innerHTML = "Empate contra:";
     }
-     enCard = $('#enemy');
+     var enCard = $('#enemy');
      enCard.fadeOut("fast",function(){
         displayCarta("enemy",cartaMaquina);
         enCard.fadeIn("slow");
@@ -117,7 +89,7 @@ var cartas = [
 
     }
   
-  }
+  });
   
   function displayCarta(id,carta){
     var elemento = '<div class="card">'+
